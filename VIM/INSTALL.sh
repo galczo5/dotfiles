bb8e71c7a3ac60fd0d9a3ef833fcc417fa9c5e6c
galczo5 <kamil.galek@gmail.com>
2015-06-26 20:49:40 +0200

    Added vim installer

---
+++ VIM/INSTALL.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "---VIM Installer---"
+echo "---galczo5/MyVimConfiguration"
+echo "---version 0.1
+
+"
+
+echo "Installation of vim, vim-gtk and git"
+#Install vim and git
+sudo apt-get install vim vim-gtk git
+
+echo "Cloning Vundle"
+#Get Vundle and place in ~/.vim/
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+echo "Cloning .vimrc"
+#Get .vimrc from my github and place it in ~/
+git clone https://github.com/galczo5/MyVimConfiguration temp 
+cp temp/.vimrc $HOME/.vimrc
+
+echo "Installing plugins"
+cd $HOME
+vim +PluginInstall +qall
+
