33f709e9005b5070322c22611af5acb3e7a68936
galczo5 <kamil.galek@gmail.com>
2016-08-07 14:15:23 +0200

    added fish config

---
+++ FISH/config.fish
@@ -0,0 +1,38 @@
+function fish_prompt --description 'Write out the prompt'
+    # Just calculate these once, to save a few cycles when displaying the prompt
+    if not set -q __fish_prompt_hostname
+    set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
+    end
+
+    if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+    end
+
+    if not set -q __git_cb
+    set __git_cb " git:"(set_color brown)(git branch ^/dev/null | grep \* | sed 's/* //')(set_color normal)""
+    end
+
+    switch $USER
+
+    case root
+
+    if not set -q __fish_prompt_cwd
+        if set -q fish_color_cwd_root
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
+        else
+            set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+        end
+    end
+
+    printf '$ %s%s%s%s '  "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+    case '*'
+
+    if not set -q __fish_prompt_cwd
+        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+    end
+
+    printf '$ %s%s%s%s ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" $__git_cb
+
+    end
+end
