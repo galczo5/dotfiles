29048ca2f8dc9132be3150ba17a904effce19a74
galczo5 <kamil.galek@gmail.com>
2025-06-18 22:55:42 +0200

    kitty config

--- FISH/config.fish
+++ FISH/config.fish
@@ -9,13 +9,13 @@ function fish_prompt
 
 	set -l branch ""
 	if git rev-parse --is-inside-work-tree >/dev/null 2>&1
-		set branch (set_color yellow --bold)(git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)(set_color normal)
+		set branch (set_color yellow --bold) (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)(set_color normal)
 	end
 
 	set -l branch (echo $branch | tr -d '\n')
-	set -l now (set_color yellow)(date "+%H:%M:%S")(set_color normal)
-	set -l currentUser (set_color yellow)$USER(set_color normal)
+	set -l currentPath (set_color blue --bold)(prompt_pwd)(set_color normal)
+	set -l currentUser (set_color blue --bold)$USER(set_color normal)
 
-	printf '%s %s %s > ' $currentUser (prompt_pwd) $branch
+	printf '%s %s %s > ' $currentUser $currentPath $branch
 
 end
