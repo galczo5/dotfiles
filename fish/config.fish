7abcdfacb43605ce25086e446b59dbf229e8444f
galczo5 <kamil.galek@gmail.com>
2025-10-26 22:29:44 +0100

    alacritty config

--- fish/config.fish
+++ fish/config.fish
@@ -9,14 +9,13 @@ function fish_prompt
 
 	set -l branch ""
 	if git rev-parse --is-inside-work-tree >/dev/null 2>&1
-		set branch (set_color yellow --bold)  (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)(set_color normal)
+		set branch (set_color red --bold)↪︎ (git symbolic-ref --short HEAD 2>/dev/null; or git rev-parse --short HEAD 2>/dev/null)(set_color normal)
 	end
 
 	set -l branch (echo $branch | tr -d '\n')
 
     set -l currentPath (set_color blue --bold)(prompt_pwd)(set_color normal)
-	set -l currentUser (set_color blue --bold)$USER(set_color normal)
 
-	printf '%s %s%s ' $currentUser $currentPath $branch 
+	printf '%s %s%s ' $currentPath $branch 
 
 end
