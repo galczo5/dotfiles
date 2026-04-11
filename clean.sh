8dcf60962b7ce8d04f05c7fc92ecf6940fabe052
galczo5 <kamil.galek@gmail.com>
2026-04-11 13:08:04 +0200

    clean.sh

---
+++ clean.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+unlink_file() {
+    local dst="$1"
+
+    if [ -L "$dst" ]; then
+        rm "$dst"
+        echo "[REMOVED] $dst (symlink)"
+    elif [ -e "$dst" ]; then
+        rm "$dst"
+        echo "[REMOVED] $dst (file)"
+    else
+        echo "[SKIP] $dst does not exist"
+    fi
+}
+
+# Fish
+unlink_file "$HOME/.config/fish/config.fish"
+
+# Ghostty
+unlink_file "$HOME/.config/ghostty/config"
+
+# Alacritty
+unlink_file "$HOME/.config/alacritty/alacritty.toml"
+
+# Kitty
+unlink_file "$HOME/.config/kitty/kitty.conf"
+
+# Lazygit
+unlink_file "$HOME/.config/lazygit/config.yml"
+
+# Midnight Commander
+unlink_file "$HOME/.config/mc/ini"
+unlink_file "$HOME/.config/mc/panels.ini"
+
+# Zed
+unlink_file "$HOME/.config/zed/settings.json"
+unlink_file "$HOME/.config/zed/keymap.json"
+unlink_file "$HOME/.config/zed/tasks.json"
+
+# Vim
+unlink_file "$HOME/.vimrc"
+
+# Vi
+unlink_file "$HOME/.exrc"
+
+# Zsh
+unlink_file "$HOME/.zshrc"
